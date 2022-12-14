70993cdffad6cf00b1ad444084fb6d0fa2ce4a56
ekafebry <ekafebry94@gmail.com>
2022-12-14 15:50:19 +0700

    add migration table || 20.

--- database/migrations/2022_12_14_065944_create_contacts_table.php
+++ database/migrations/2022_12_14_065944_create_contacts_table.php
@@ -15,6 +15,11 @@ class CreateContactsTable extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('subject')->nullable();
+            $table->text('message')->nullable();
             $table->timestamps();
         });
     }
